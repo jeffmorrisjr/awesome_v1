b82599428ae02feaf705fb8ff63344d6054f62ce
jeffmorrisjr <jeff@zaarly.com>
2014-12-15 12:49:48 -0800

    setup views and public

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var express = require("express"),
-  bodyParser = require("bodyParser"),
+  bodyParser = require("body-parser"),
   app = express();
 
 
+app.use(express.static(__dirname+ "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 // our root route
-app.get("/", function) (req, res) {
+app.get("/", function (req, res) {
 	res.send("Hello world");
 });
 
 app.listen(3000, function () {
-	console.log(new Array(*).join());
-	console.log(STARTED ON localhost3000)
+	console.log(new Array(50).join('*'));
+	console.log("STARTED ON localhost:3000");
 })
