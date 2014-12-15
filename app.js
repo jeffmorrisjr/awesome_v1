8e0cb4bb510c39610858e5b640181bb125602255
jeffmorrisjr <jeff@zaarly.com>
2014-12-15 12:29:19 -0800

    setup simple app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express"),
+  bodyParser = require("bodyParser"),
+  app = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+// our root route
+app.get("/", function) (req, res) {
+	res.send("Hello world");
+});
+
+app.listen(3000, function () {
+	console.log(new Array(*).join());
+	console.log(STARTED ON localhost3000)
+})
